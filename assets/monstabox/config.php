53a860f3db65f685b1e9d37186ba95f1bdbf75a9
sansusan <sansusan@yandex.ru>
2016-03-15 11:24:04 +0300

    Init

---
+++ assets/monstabox/config.php
@@ -0,0 +1,21 @@
+<?php
+
+# Open README file for descriptions and help.
+
+$ftpHost = "";
+$ftpPort = "21";
+$ftpMode = "1";
+$ftpSSL  = "0";
+$ftpDir  = "";
+$serverTmp = "/tmp"; // Linux
+// $serverTmp = "C:\\WINDOWS\\Temp\\"; // Windows
+$editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml";
+$dateFormatUsa = 0;
+$lockOutTime = 5;
+$versionCheck = 0;
+$showAdvOption = 0;
+$showLockSess = 0;
+$showHostInfo = 1;
+$showAddons = 0;
+
+?>
